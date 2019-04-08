61f447907eced20b1783de4b6e2f0c848b394310
Robert Cai <robert913152750@gmail.com>
2019-04-08 18:33:34 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//require packages uesed in the project
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//require express-handlebars here
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+
+//settting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//setting static files
+app.use(express.static("public"));
+
+//routes setting
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.filter(
+    restaurant => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant[0] });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurantName = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
+  });
+  const restaurantNameCategory = restaurantList.results.filter(restaurant => {
+    return restaurant.category.toLowerCase().includes(keyword.toLowerCase());
+  });
+  const restaurants = restaurantName.concat(restaurantNameCategory);
+  console.log(restaurants);
+  res.render("index", { restaurants: restaurants, keyword: keyword });
+});
+
+//start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
